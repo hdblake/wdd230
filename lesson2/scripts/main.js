f0c21baf36aefda18c10b8d75aa906af2acc4624
schmunt <hdblake333@gmail.com>
2022-10-01 00:23:32 -0500

    updated js to locale string

--- lesson2/scripts/main.js
+++ lesson2/scripts/main.js
@@ -4,4 +4,7 @@ let year = d.getFullYear();
 document.querySelector("#current-year").innerHTML = year;
 
 let dateModified = new Date(document.lastModified);
-document.querySelector("#last-update").innerHTML = dateModified;
+let date = dateModified.toLocaleString('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'});
+let time = dateModified.toLocaleString('en-GB', {hour: '2-digit', minute: '2-digit', second: '2-digit'});
+let fullDate = `Last Updated: ${date} ${time}`;
+document.querySelector("#last-update").innerHTML = fullDate;
