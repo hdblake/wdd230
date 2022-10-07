003605224c1940c4fb23c0aa4e80783461fa3a3d
= <=>
2022-10-07 16:29:44 -0500

    added script for hamburger button and footer

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,8 +1,24 @@
+// Script for current date in Header
 const headerDate = document.querySelector("#current-date");
-
 const current = new Date();
 const fullDate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(
   current
 );
-
 headerDate.innerHTML = `${fullDate}`;
+
+// Script for hamburger menu button in small/medium css view
+function toggleMenu() {
+  document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
+}
+
+// Script for footer
+const d = new Date();
+
+let year = d.getFullYear();
+document.querySelector("#current-year").innerHTML = year;
+
+let dateModified = new Date(document.lastModified);
+let date = dateModified.toLocaleString('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'});
+let time = dateModified.toLocaleString('en-GB', {hour: '2-digit', minute: '2-digit', second: '2-digit'});
+let full = `Last Updated: ${date} ${time}`;
+document.querySelector("#last-update").innerHTML = full;
