134256686962e2ed137435f629aba957dfc08393
= <=>
2022-10-22 21:47:28 -0500

    fixed script for displaying windchill (works)

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,15 +1,15 @@
-let temp = document.querySelector(".temp-in-f");
-let windSpeed = document.querySelector(".speed");
+let temp = document.querySelector(".temp-in-f").innerHTML;
+let windSpeed = document.querySelector(".speed").innerHTML;
 let windChill = document.querySelector(".chill");
 
-if (temp.innerHTML <= '50' && windSpeed.innerHTML > '3') {
+if (temp <= 50 && windSpeed > 3) {
   let chill =
     35.74 +
-    0.6215(temp) -
-    35.75(windSpeed ** 0.16) +
-    0.4275(temp)(windSpeed ** 0.16);
+    0.6215 * temp -
+    35.75 * windSpeed ** 0.16 +
+    0.4275 * temp * windSpeed ** 0.16;
 
-  windChill.innerHTML = `${chill}`;
+  windChill.innerHTML = `${Math.round(chill)}`;
 } else {
   chill = "N/A";
 
