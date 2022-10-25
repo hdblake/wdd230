75097e24c4446e03126b644e3374c68fb8b6c3e2
= <=>
2022-10-25 12:36:26 -0500

    added script for meet and greet banner

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -7,12 +7,16 @@ const fullDate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(
 headerDate.innerHTML = `${fullDate}`;
 
 // Script for hamburger menu button in small/medium css view
-const mainNav = document.querySelector('.navigation');
+const mainNav = document.querySelector(".navigation");
 function toggleMenu() {
-  document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
+  document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
 }
 
-window.onresize = () => {if (window.innerWidth > 991) mainNav.classList.remove('responsive')};
+window.onresize = () => {
+  if (window.innerWidth > 991) mainNav.classList.remove("responsive");
+};
 
 // Script for footer
 const d = new Date();
@@ -21,7 +25,21 @@ let year = d.getFullYear();
 document.querySelector("#current-year").innerHTML = year;
 
 let dateModified = new Date(document.lastModified);
-let date = dateModified.toLocaleString('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'});
-let time = dateModified.toLocaleString('en-GB', {hour: '2-digit', minute: '2-digit', second: '2-digit'});
+let date = dateModified.toLocaleString("en-US", {
+  month: "2-digit",
+  day: "2-digit",
+  year: "numeric",
+});
+let time = dateModified.toLocaleString("en-GB", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+});
 let full = `Last Updated: ${date} ${time}`;
 document.querySelector("#last-update").innerHTML = full;
+
+// Script for meet and greet banner
+const banner = document.querySelector(".meet-greet");
+if (current.getDay() === 1 || current.getDay() === 2) {
+  banner.style.display = "block";
+}
