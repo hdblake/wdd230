6d5e9ffa29e3d0bfd26e8d9b41dd0a3245b5f4fb
= <=>
2022-10-26 15:30:21 -0500

    started script

---
+++ lesson7/app.js
@@ -0,0 +1,7 @@
+let imageToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+	image.setAttribute("src", image.getAttribute("data-src"));
+	image.onload = () => {
+		image.removeAttribute("data-src");
+	};
+};
