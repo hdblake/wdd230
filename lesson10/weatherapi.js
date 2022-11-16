cbc57e577b5adee20f38ef7cb59694fc5cc7b764
= <=>
2022-11-16 15:34:17 -0600

    start of weather api project

---
+++ lesson10/weatherapi.js
@@ -0,0 +1,6 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const URL =
+	"https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,us&appid=97ce60e6d0bc0ca722b497935512500d";
