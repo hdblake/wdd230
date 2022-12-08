7e0175ef1b27b197ab3a86f9af14985681af8553
= <=>
2022-12-08 16:36:47 -0600

    updated variables and count for days

--- final-project/scripts/weatherAPI.js
+++ final-project/scripts/weatherAPI.js
@@ -1,15 +1,17 @@
+let currentForecast = document.createElement("div");
 let currentTemp = document.createElement("p");
 let conditions = document.createElement("p");
 let humidity = document.createElement("p");
 let forecast = document.createElement("div");
-let currentHigh = document.createElement("p");
+let forecastH4 = document.createElement("h4");
+let dayOneHigh = document.createElement("p");
 let dayTwoHigh = document.createElement("p");
 let dayThreeHigh = document.createElement("p");
 let card = document.querySelector(".weather-card");
 let display = document.querySelector(".weather");
 
 const URL =
-	"https://api.openweathermap.org/data/2.5/forecast?lat=33.1581&lon=-117.3506&cnt=3&appid=97ce60e6d0bc0ca722b497935512500d&units=imperial";
+	"https://api.openweathermap.org/data/2.5/forecast?lat=33.1581&lon=-117.3506&cnt=4&appid=97ce60e6d0bc0ca722b497935512500d&units=imperial";
 
 async function fetchAPI() {
 	try {
@@ -30,30 +32,38 @@ fetchAPI();
 
 function displayWeather(weatherData) {
 	currentTemp.innerHTML = `Current Temp: ${weatherData.list[0].main.temp.toFixed(0)}°F`;
-	card.appendChild(currentTemp);
+	currentForecast.appendChild(currentTemp);
 
 	let currentCondition = weatherData.list[0].weather[0].description;
 	currentCondition = currentCondition.toLowerCase().split(" ")
 	.map((a) => a.charAt(0).toUpperCase() + a.substring(1)).join(" ");
 
 	conditions.innerHTML = `${currentCondition}`;
-	card.appendChild(conditions);
+	currentForecast.appendChild(conditions);
 
 	humidity.innerHTML = `Humidity: ${weatherData.list[0].main.humidity}%`;
-	card.appendChild(humidity);
+	currentForecast.appendChild(humidity);
+	card.appendChild(currentForecast);
 
-	let dayTwoCondition = weatherData.list[1].weather[0].description;
+	let dayOneCondition = weatherData.list[1].weather[0].description;
+	dayOneCondition = dayOneCondition.toLowerCase().split(" ")
+	.map((a) => a.charAt(0).toUpperCase() + a.substring(1)).join(" ");
+
+	let dayTwoCondition = weatherData.list[2].weather[0].description;
 	dayTwoCondition = dayTwoCondition.toLowerCase().split(" ")
 	.map((a) => a.charAt(0).toUpperCase() + a.substring(1)).join(" ");
 
-	let dayThreeCondition = weatherData.list[2].weather[0].description;
+	let dayThreeCondition = weatherData.list[3].weather[0].description;
 	dayThreeCondition = dayThreeCondition.toLowerCase().split(" ")
 	.map((a) => a.charAt(0).toUpperCase() + a.substring(1)).join(" ");
 
-	currentHigh.innerHTML = `Day 1 High (Today): ${weatherData.list[0].main.temp_max.toFixed(0)}°F`;
-	dayTwoHigh.innerHTML = `Day 2 High: ${weatherData.list[1].main.temp_max.toFixed(0)}°F and ${dayTwoCondition}`;
-	dayThreeHigh.innerHTML = `Day 3 High: ${weatherData.list[2].main.temp_max.toFixed(0)}°F and ${dayThreeCondition}`;
-	forecast.appendChild(currentHigh);
+	forecastH4.innerHTML = `Three Day Forcast`;
+	forecast.appendChild(forecastH4);
+
+	dayOneHigh.innerHTML = `Day 1 High: <br>${weatherData.list[1].main.temp_max.toFixed(0)}°F <br>${dayOneCondition}`;
+	dayTwoHigh.innerHTML = `Day 2 High: <br>${weatherData.list[2].main.temp_max.toFixed(0)}°F <br>${dayTwoCondition}`;
+	dayThreeHigh.innerHTML = `Day 3 High: <br>${weatherData.list[3].main.temp_max.toFixed(0)}°F <br>${dayThreeCondition}`;
+	forecast.appendChild(dayOneHigh);
 	forecast.appendChild(dayTwoHigh);
 	forecast.appendChild(dayThreeHigh);
 	card.appendChild(forecast);
