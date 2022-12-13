11f70b8cb271478ece44c3c000d5198888f14083
= <=>
2022-12-13 15:10:34 -0600

    commeneted out code for now

--- final-project/scripts/drink-ls.js
+++ final-project/scripts/drink-ls.js
@@ -1,15 +1,15 @@
 // Locallly store number of drinks ordered
-let drinkDisplay = document.querySelector(".total-orders");
-let drinksOrdered = Number(window.localStorage.getItem("drinks-ls"));
+// let drinkDisplay = document.querySelector(".total-orders");
+// let drinksOrdered = Number(window.localStorage.getItem("drinks-ls"));
 
-if (drinksOrdered !== 0) {
-  drinkDisplay.innerHTML = `Drinks Ordered: ${drinksOrdered}`;
-} else {
-  drinkDisplay.innerHTML = `Drinks Ordered: `;
-}
+// if (drinksOrdered) {
+//   drinkDisplay.textContent = JSON.parse();
+// } else {
+//   drinkDisplay.innerHTML = `Drinks Ordered: `;
+// }
 
-// Increment number of drink orders
-drinksOrdered++;
+// // Increment number of drink orders
+// drinksOrdered++;
 
-// Store number of drinks ordered
-localStorage.setItem("drinks-ls", drinksOrdered);
+// // Store number of drinks ordered
+// localStorage.setItem("drinks-ls", drinksOrdered);
