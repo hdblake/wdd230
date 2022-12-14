60b91009ccfff89c0d5693a7054156d9c2b4598c
schmunt <hdblake333@gmail.com>
2022-12-13 22:56:50 -0600

    updated with comments

--- final-project/scripts/drink-form.js
+++ final-project/scripts/drink-form.js
@@ -1,4 +1,5 @@
 const requestURL = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+let drinks = [];
 
 async function fetchFruitApi() {
   try {
@@ -43,8 +44,6 @@ function displayFruits(content) {
   }
 }
 
-// displayOrder should SUM carbs, protein, fat, sugar, and calories for drink based upon 3 selections
-
 let button = document.getElementById("submit");
 
 button.addEventListener('click', displayOrder);
@@ -53,6 +52,7 @@ function displayOrder(event) {
 
   event.preventDefault();
 
+  // Get input values from form
   let name = document.getElementById("fname").value;
   let email = document.getElementById("email").value;
   let phone = document.getElementById("phone").value;
@@ -63,18 +63,43 @@ function displayOrder(event) {
   let output = document.querySelector("#output");
   const date = new Date();
 
+  // Store input values in object
+  const drink = {
+    name: name,
+    email: email,
+    phone: phone,
+    option1: fruit1,
+    option2: fruit2,
+    option3: fruit3,
+    request: request,
+    carbohydrates: 0,
+    protein: 0,
+    fat: 0,
+    sugar: 0,
+    calories: 0
+  };
+
   // Variable for modified date format
   let dateFormat = date.toLocaleString("en-US", {
     month: "2-digit", day: "2-digit", year: "numeric"
   });
 
+  drinks.push(drink);
 
+  // Add input values to output area
   output.innerHTML = `<p>Order Submitted! Details:<br>
   First Name: ${name}<br>
   Email: ${email}<br>
   Phone Number: ${phone}<br>
   Fruit Selections: ${fruit1}, ${fruit2}, ${fruit3}<br>
   Request: ${request}<br>
-  Date: ${dateFormat}</p>`;
-  
+  Date: ${dateFormat}</p>
+  <p>Carbs: ${drink.carbohydrates}<br>
+  Protein: ${drink.protein}<br>
+  Fat: ${drink.fat}<br>
+  Sugar: ${drink.sugar}<br>
+  Calories: ${drink.calories}</p>`;
+
+  // Locally store data in drinks variable
+  localStorage.setItem("drinks", JSON.stringify(drinks));
 }
